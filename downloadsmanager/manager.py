f78b792a39f2b5ff7822f5613f86df9050a40c61
HeikkiLu <heikki.luukkonen@gmail.com>
2021-03-02 15:09:52 +0200

    Different function for moving files

--- downloadsmanager/manager.py
+++ downloadsmanager/manager.py
@@ -6,33 +6,33 @@ home = str(Path.home())
 downloads_folder = home+"/Downloads/"
 
 # List all files in downloads folder
-def move_files():
+def list_files():
+
     files = os.listdir(downloads_folder)
     
     for f in files:
         ext = Path(f).suffix
         try:
             if ext=='.md' or ext=='.pdf' or ext=='.docx' or ext=='.data' or ext=='.pptx':
-                new_path = shutil.move(downloads_folder + f, f'{downloads_folder}documents')
-                print(f'Moved {downloads_folder + f} to {new_path}')
+                move_file(f, 'documents')   
             if ext=='.zip' or ext=='.jar' or ext=='.tar' or ext=='.gz' or ext=='.rar':
-                new_path = shutil.move(downloads_folder + f, f'{downloads_folder}zips')
-                print(f'Moved {downloads_folder + f} to {new_path}')
+                move_file(f, 'zips')
             if ext=='.jpg' or ext=='.jpeg' or ext=='.png' or ext=='.tiff' or ext=='.gif':
-                new_path = shutil.move(downloads_folder + f, f'{downloads_folder}pictures')
-                print(f'Moved {downloads_folder + f} to {new_path}')
+                move_file(f, 'pictures')
             if ext=='.exe' or ext=='.deb' or ext=='.AppImage' or ext=='.sh' or ext=='.run':
-                new_path = shutil.move(downloads_folder + f, f'{downloads_folder}executables')
-                print(f'Moved {downloads_folder + f} to {new_path}')
+                move_file(f, 'executables')
             else:
                 if f=="documents" or f=="zips" or f=="pictures" or f=="executables" or f=="other":
                     pass
                 else:
-                    new_path = shutil.move(downloads_folder + f, f'{downloads_folder}other')
-                    print(f'Moved {downloads_folder + f} to {new_path}')
-
+                    move_file(f, 'other')
         except FileNotFoundError:
             pass
+
+# Move files to their directories
+def move_file(f, d):
+    new_path = shutil.move(downloads_folder + f, downloads_folder+d)
+    print(f'Moved {downloads_folder + f} to {new_path}')
 
 # Make directories if not present
 def make_dirs():
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     print("Checking if directories exist.")
     make_dirs()
     print("Moving files.")
-    move_files()
+    list_files()
